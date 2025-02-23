bcf604bc970dfb2d6259814192342f5f14b15593
Christian Berclaz <ch.berclaz@gmail.com>
2025-02-23 18:14:52 +0100

    implemented multiheader and feedforwarder

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ batch_size = 32  # how many independent sequences will we process in parallel
 block_size = 8  # what is the maximalum context length for predictions
 max_iters=5000
 eval_interval= 300
-lerning_rate= 1e-2
+lerning_rate= 1e-3
 device= 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters=200
 n_embd=32
@@ -85,6 +85,50 @@ def estimate_loss():
     return out
 
 
+class Head(nn.Module):
+    # one Head of self-attention
+    def __init__(self, head_size):
+        super().__init__()
+        self.key= nn.Linear(n_embd, head_size, bias=False)
+        self.query= nn.Linear(n_embd, head_size, bias=False)
+        self.value= nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k= self.key(x)   #(B,T,16)
+        q= self.query(x) #(B,T,16)
+        v = self.value(x) # --> here is what im interested in, here is what i have and if you find me interesting, this is what i will communicate with you
+ 
+        #compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1)* C**-0.5 # (B,T,C) @ (B,C,T) --> (B,T,T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))   # elements can only look in the past --> decoder Block (B,T,T)
+        wei = F.softmax(wei, dim=-1) # (B,T,T)
+
+        # perform the weighted aggregation of the values
+        out = wei @ v # (B,T,T) @ (B,T,C) --> (B,T,C) # --> Adding Values depending on how interesting the elements find each other (Q,K,V)
+        return out
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1) # concatenate over the chanel dimension
+    
+class FeedForward(nn.Module):
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
 # super simpel Bigram Model
 # see makemore video series of andrej for more informations
 class BigramLanguageModel(nn.Module):
@@ -94,6 +138,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table= nn.Embedding(block_size, n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4) # i.e 4 heads of 8 dimensional self-attention
+        self.fwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd,vocab_size)
 
     def forward(self, idx, targets=None):
@@ -102,6 +148,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) # (B,T,C) --> batch by time by chanel (chanel = vocab_size)
         pos_emb= self.position_embedding_table(torch.arange(T,device=device)) # (T,C) 
         x = tok_emb + pos_emb # (B,T,C) --> not only token identity but also position at which they accur
+        x = self.sa_heads(x) # apply one head of self-attention (B,T,C)
+        x = self.fwd(x) # think on trained data
         logits= self.lm_head(x) #(B,T,Vocab_size)
 
         if targets is None:
@@ -118,8 +166,10 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current  context
         for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
             #get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes (b,C)
             # applay softmax to get probabilities
