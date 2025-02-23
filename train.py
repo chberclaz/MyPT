e29abc302ec77eae3b829194fe2812fbb472915b
Christian Berclaz <ch.berclaz@gmail.com>
2025-02-23 19:13:43 +0100

    added dropout and resized variable definitions (blocksize from 8 to 256)

--- train.py
+++ train.py
@@ -8,14 +8,18 @@ from torch.nn import functional as F
 torch.manual_seed(1337)
 
 #hyperparameters
-batch_size = 32  # how many independent sequences will we process in parallel
-block_size = 8  # what is the maximalum context length for predictions
+batch_size = 64  # how many independent sequences will we process in parallel
+block_size = 256  # what is the maximalum context length for predictions
 max_iters=5000
 eval_interval= 300
-lerning_rate= 1e-3
+lerning_rate= 3e-4
 device= 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters=200
-n_embd=32
+n_embd=384
+n_head = 6
+n_layer = 6
+dropout = 0.2
+
 # -------------------------------------------------------------------------------------------------------------------
 # input: textfile
 # plain text from a author (in our case sharespeare)
@@ -93,6 +97,7 @@ class Head(nn.Module):
         self.query= nn.Linear(n_embd, head_size, bias=False)
         self.value= nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B,T,C = x.shape
@@ -104,7 +109,7 @@ class Head(nn.Module):
         wei = q @ k.transpose(-2,-1)* C**-0.5 # (B,T,C) @ (B,C,T) --> (B,T,T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))   # elements can only look in the past --> decoder Block (B,T,T)
         wei = F.softmax(wei, dim=-1) # (B,T,T)
-
+        wei = self.dropout(wei)
         # perform the weighted aggregation of the values
         out = wei @ v # (B,T,T) @ (B,T,C) --> (B,T,C) # --> Adding Values depending on how interesting the elements find each other (Q,K,V)
         return out
@@ -113,17 +118,23 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1) # concatenate over the chanel dimension
+        out=torch.cat([h(x) for h in self.heads], dim=-1) # concatenate over the chanel dimension
+        out=self.dropout(self.proj(out))
+        return out
     
 class FeedForward(nn.Module):
 
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4*n_embd),
             nn.ReLU(),
+            nn.Linear(4*n_embd, n_embd),
+            nn.Dropout(dropout),
         )
 
     def forward(self, x):
@@ -138,10 +149,12 @@ class Block(nn.Module):
         head_size = n_embd // n_head
         self.sa = MultiHeadAttention(n_head, head_size)
         self.fwd= FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
 
     def forward(self,x):
-        x = self.sa(x)
-        x= self.fwd(x)
+        x = x + self.sa(self.ln1(x))
+        x = x + self.fwd(self.ln2(x))
         return x
 
 # super simpel Bigram Model
@@ -153,11 +166,8 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table= nn.Embedding(block_size, n_embd)
-        self.blocks= nn.Sequential(
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-            Block(n_embd, n_head=4),
-        )
+        self.blocks= nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f= nn.LayerNorm(n_embd) # final Layer norm
         self.lm_head = nn.Linear(n_embd,vocab_size)
 
     def forward(self, idx, targets=None):
@@ -167,6 +177,7 @@ class BigramLanguageModel(nn.Module):
         pos_emb= self.position_embedding_table(torch.arange(T,device=device)) # (T,C) 
         x = tok_emb + pos_emb # (B,T,C) --> not only token identity but also position at which they accur
         x = self.blocks(x)
+        x = self.ln_f(x)
         logits= self.lm_head(x) #(B,T,Vocab_size)
 
         if targets is None:
